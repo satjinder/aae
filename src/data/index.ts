c350a3a52f713b8d77db9ae083a01ee0089bdf4b
satjinder bath <satjinder@gmail.com>
2025-06-15 21:37:16 +1000

    added some fake systems and teams

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,16 +1,21 @@
 import { allBusinessAreaNodes, allBusinessAreaEdges } from './business_areas';
 import { allApiNodes, allApiEdges, allEventNodes, allEventEdges } from './apis';
+import { allSystemNodes, allTeamNodes, allSystemEdges, allTeamEdges } from './systems';
 
 // Combine all nodes
 export const allNodes = [
   ...allBusinessAreaNodes,
   ...allApiNodes.apis,
-  ...allEventNodes.events
+  ...allEventNodes.events,
+  ...allSystemNodes.nodes,
+  ...allTeamNodes.nodes
 ];
 
 // Combine all edges
 export const allEdges = [
   ...allBusinessAreaEdges,
   ...allApiEdges.nodes,
-  ...allEventEdges.nodes
+  ...allEventEdges.nodes,
+  ...allSystemEdges.nodes,
+  ...allTeamEdges.nodes
 ]; 
---
+++ src/data/systems/index.ts
@@ -0,0 +1,9 @@
+import systems from './systems.json';
+import systemEdges from './system_edges.json';
+import teams from './teams.json';
+import teamEdges from './team_edges.json';
+
+export const allSystemNodes = systems;
+export const allSystemEdges = systemEdges;
+export const allTeamNodes = teams;
+export const allTeamEdges = teamEdges; 
